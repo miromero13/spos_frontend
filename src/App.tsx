edb6ba1eeea7b5e6f8742b879d4d62cb39917f02
Ilse Romero <ilseromero35@gmail.com>
2025-04-19 23:04:09 -0400

    fix: update project name to spos_frontend and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,19 @@ import { AuthProvider } from './context/authContext'
 import { store } from './redux/store'
 import Routes from './routes'
 import { ThemeProvider } from './context/themeContext'
-// import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function App() {
-  // 291724056968-k5rlgcpdifrukmb7asu2loc10440or1b.apps.googleusercontent.com
   return (
     <Provider store={store}> {/* para las acciones y el estado */}
-      {/* <GoogleOAuthProvider clientId="291724056968-k5rlgcpdifrukmb7asu2loc10440or1b.apps.googleusercontent.com"> */}
-        <AuthProvider>
-          <ThemeProvider>
-            <SWRConfig value={{ revalidateOnFocus: false }}> {/* para las peticiones de la api */}
-              <BrowserRouter> {/* para las rutas */}
-                <Routes /> {/* rutas */}
-              </BrowserRouter>
-            </SWRConfig>
-          </ThemeProvider>
-        </AuthProvider>
-      {/* </GoogleOAuthProvider> */}
+      <AuthProvider>
+        <ThemeProvider>
+          <SWRConfig value={{ revalidateOnFocus: false }}> {/* para las peticiones de la api */}
+            <BrowserRouter> {/* para las rutas */}
+              <Routes /> {/* rutas */}
+            </BrowserRouter>
+          </SWRConfig>
+        </ThemeProvider>
+      </AuthProvider>
     </Provider>
   )
 }
