24e6e65af899ba2a657ed56c1d33485589fbfd1d
Luis Gabriel Janco <sauterdevs@gmail.com>
2025-04-21 23:42:44 -0400

    feat(products): add SalesPage route with permissions for admin

--- src/routes/utils/products.utils.ts
+++ src/routes/utils/products.utils.ts
@@ -7,6 +7,7 @@ const ProductFormPage = lazy(() => import('@modules/sales/pages/products/compone
 const CashPage = lazy(() => import('@modules/sales/pages/cashes'))
 const CashControlPage = lazy(() => import('@modules/sales/pages/cashes/components/cash-control'))
 const SalePage = lazy(() => import('@modules/sales/pages/sales'))
+const SalesPage = lazy(() => import('@modules/sales/pages/sales/sales-page'))
 
 export const productRoutes: Route[] = [
   {
@@ -38,5 +39,10 @@ export const productRoutes: Route[] = [
     path: PrivateRoutes.SALE_CREATE,
     element: createElement(SalePage),
     permissions: [PERMISSION.ADMIN, PERMISSION.CASHIER] as PERMISSION[]
+  },
+  {
+    path: PrivateRoutes.SALE,
+    element: createElement(SalesPage),
+    permissions: [PERMISSION.ADMIN] as PERMISSION[]
   }
 ]
